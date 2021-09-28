d180c447987a1e4bf358942d3f4dd4c99919525b
Colin Wilson <github@colinwilson.dev>
2021-09-28 09:14:55 -0400

    Rating updates correctly

--- functions/index.js
+++ functions/index.js
@@ -15,78 +15,31 @@ exports.adjustRating = functions.firestore
 
     const data = snapshot.data();
 
-    return firestore.runTransaction((transaction) => {
+    return firestore.runTransaction(async (transaction) => {
       const docRefs = [];
       data.winners.forEach(function (player) {
         docRefs.push(firestore.doc("players/" + player._id));
       });
-      return transaction.getAll(...docRefs).then((doc) => {
-        if (doc.exists) {
-          const rating = doc.get("rating");
-          transaction.update(doc.ref, { rating: rating + 10 });
-        }
+      data.losers.forEach(function (player) {
+        docRefs.push(firestore.doc("players/" + player._id));
+      });
+      return transaction.getAll(...docRefs).then((docs) => {
+        docs.forEach((doc) => {
+          if (doc.exists) {
+            const rating = doc.data().rating;
+            let isInWinners = false;
+            data.winners.forEach(function (player) {
+              if (player._id == doc.id) {
+                isInWinners = true;
+              }
+            });
+            if (isInWinners) {
+              transaction.update(doc.ref, { rating: rating + 10 });
+            } else {
+              transaction.update(doc.ref, { rating: rating - 10 });
+            }
+          }
+        });
       });
     });
-
-    // firestore.runTransaction((transaction) => {
-    //   const promises = [];
-
-    //   data.winners.forEach(function (player) {
-    //     const documentRef = firestore.doc("players/" + player._id);
-    //     promises.push(
-    //       transaction.get(documentRef).then((doc) => {
-    //         if (doc.exists) {
-    //           const rating = doc.get("rating");
-    //           transaction.update(documentRef, { rating: rating + 10 });
-    //         }
-    //       })
-    //     );
-    //   });
-
-    //   data.losers.forEach(function (player) {
-    //     const documentRef = firestore.doc("players/" + player._id);
-    //     promises.push(
-    //       transaction.get(documentRef).then((doc) => {
-    //         if (doc.exists) {
-    //           const rating = doc.get("rating");
-    //           transaction.update(documentRef, { rating: rating - 10 });
-    //         }
-    //       })
-    //     );
-    //   });
-
-    //   return Promise.all(promises);
-    // });
-
-    // data.winners.forEach(function (player) {
-    //   firestore.runTransaction((transaction) => {
-    //     const documentRef = firestore.doc("players/" + player._id);
-    //     return transaction.get(documentRef).then((doc) => {
-    //       if (doc.exists) {
-    //         const rating = doc.data().rating;
-    //         transaction.set(
-    //           documentRef,
-    //           { rating: rating + 10 },
-    //           { merge: true }
-    //         );
-    //       }
-    //     });
-    //   });
-    // });
-
-    // data.losers.forEach(function (player) {
-    //   firestore.runTransaction((transaction) => {
-    //     const documentRef = firestore.doc("players/" + player._id);
-    //     return transaction.get(documentRef).then((doc) => {
-    //       if (doc.exists) {
-    //         const rating = doc.data().rating;
-    //         transaction.set(
-    //           documentRef,
-    //           { rating: rating - 10 },
-    //           { merge: true }
-    //         );
-    //       }
-    //     });
-    //   });
-    // });
   });
